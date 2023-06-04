b432103575f9e77f8e2ac620125e7ec7fddacd12
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-06-04 11:36:48 +0900

    feat: zod를 이용한 Character, List, Detail Schema

---
+++ src/types/character.ts
@@ -0,0 +1,17 @@
+import z from 'zod';
+
+export const CharacterSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  imageUrl: z.string(),
+});
+export type Character = z.infer<typeof CharacterSchema>;
+
+export const CharacterListSchema = z.array(CharacterSchema);
+export type CharacterList = z.infer<typeof CharacterListSchema>;
+
+export const CharacterDetailSchema = CharacterSchema.extend({
+  films: z.array(z.string()),
+  sourceUrl: z.string(),
+});
+export type CharacterDetail = z.infer<typeof CharacterDetailSchema>;
