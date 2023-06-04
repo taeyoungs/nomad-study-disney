2b5d13a7c44616ad3cd0bef2bc1fc43c58ec5b78
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-06-04 11:06:03 +0900

    feat: QueryClientProvider, QueryClient 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RouterProvider } from 'react-router-dom';
+
+import router from './router';
+
+const queryClient = new QueryClient();
+
 function App() {
-  return <div>App</div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
