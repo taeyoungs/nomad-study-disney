0d3fbf4f70759b017cec0f41cb6aa0901edd6b2f
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-06-04 14:19:51 +0900

    feat: Home 페이지 컴포넌트 업데이트
    pages/Home/index.tsx
    - Suspense와 ErrorBoundary를 이용한 디즈니 캐릭터 리스트
      렌더링(CharacterList 컴포넌트)
    
    index.css
    - a 태그 text-decoration none 처리

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,30 @@
-import { Text } from 'components/common';
+import { Suspense } from '@suspensive/react';
+import { QueryErrorBoundary } from '@suspensive/react-query';
+import styled from 'styled-components';
+
+import { Text, ErrorBoundaryFallback } from 'components';
 
 import { color } from 'constants/common';
+
+import { CharacterList, CharacterListFallback } from './components';
+
+const Wrapper = styled.div`
+  margin: 0 0 20px;
+`;
 
 function Home() {
   return (
     <section>
-      <Text as="h1" typography="3xlarge" color={color.white} align="center">
-        Disney Characters
-      </Text>
+      <Wrapper>
+        <Text as="h1" typography="3xlarge" color={color.white} align="center">
+          Disney Characters
+        </Text>
+      </Wrapper>
+      <QueryErrorBoundary fallback={({ reset }) => <ErrorBoundaryFallback reset={reset} />}>
+        <Suspense fallback={<CharacterListFallback />}>
+          <CharacterList />
+        </Suspense>
+      </QueryErrorBoundary>
     </section>
   );
 }
